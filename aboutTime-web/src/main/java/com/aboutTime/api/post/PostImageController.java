3deaa7717818eb93afeece8ffbe885869a0ea4e5
kiteB <mmy789@sch.ac.kr>
2024-08-13 15:26:38 +0900

    feat: S3 이미지 업로드 기능 초안

---
+++ aboutTime-web/src/main/java/com/aboutTime/api/post/PostImageController.java
@@ -0,0 +1,34 @@
+package com.aboutTime.api.post;
+
+import com.aboutTime.api.docs.swagger.PostImageControllerDocs;
+import com.aboutTime.service.post.PostImageService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/api/post")
+@RequiredArgsConstructor
+public class PostImageController implements PostImageControllerDocs {
+
+    private final PostImageService imageService;
+
+    @PostMapping(path = "/image/upload",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<String> uploadImage(@RequestParam("image") MultipartFile file,
+                                                 @RequestParam("id") Long postId,
+                                                 @RequestParam(value = "comment", required = false) String comment) {
+        var imageUri = imageService.uploadImage(postId, file, comment);
+        return ResponseEntity.ok(imageUri);
+    }
+
+    @DeleteMapping("/image/remove")
+    public ResponseEntity<Void> removeImage(Long postImageId) {
+        imageService.deleteImage(postImageId);
+        return ResponseEntity.noContent().build();
+    }
+
+}
