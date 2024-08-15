db91c3681d00bd04492d2fce01f80ccbba8a64bc
Jung Hyun kee <jhkman1208@gmail.com>
2024-08-15 13:07:47 +0900

    fix: UserOauth 및 User 오류 수정

--- aboutTime-application/src/main/java/com/aboutTime/infra/user/oauth/service/OAuthUserService.java
+++
@@ -1,9 +0,0 @@
-package com.aboutTime.infra.user.oauth.service;
-
-import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Service;
-
-@Service
-@RequiredArgsConstructor
-public class OAuthUserService {
-}
