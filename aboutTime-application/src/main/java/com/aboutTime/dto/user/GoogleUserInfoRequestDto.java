d557776f2c2bf938667dd0a88a91b85fd0d2fac0
Jung Hyun kee <jhkman1208@gmail.com>
2024-09-05 17:29:02 +0900

    feat: Google OAuth 인증 관련 dto 추가

---
+++ aboutTime-application/src/main/java/com/aboutTime/dto/user/GoogleUserInfoRequestDto.java
@@ -0,0 +1,19 @@
+package com.aboutTime.dto.user;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@AllArgsConstructor
+@Getter
+@Setter
+public class GoogleUserInfoRequestDto {
+    public String id;
+    public String email;
+    public Boolean verifiedEmail;
+    public String name;
+    public String givenName;
+    public String familyName;
+    public String picture;
+    public String locale;
+}
