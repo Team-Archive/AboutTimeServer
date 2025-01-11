2fb429b9356d3f4092873658917f78e98b8c8774
Jung Hyun Kee <jhkman1208@gmail.com>
2025-01-11 12:27:38 +0900

    feat : 회원정보 조회 API 추가

---
+++ aboutTime-web/src/main/java/com/aboutTime/api/user/UserController.java
@@ -0,0 +1,27 @@
+package com.aboutTime.api.user;
+
+import com.aboutTime.api.docs.swagger.UserControllerDocs;
+import com.aboutTime.dto.user.BaseUserDto;
+import com.aboutTime.service.user.UserService;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@Tag(name = "user-controller", description = "회원정보 관련 API 목록")
+@RequestMapping("/api/user")
+@RequiredArgsConstructor
+public class UserController implements UserControllerDocs {
+
+    private final UserService userService;
+
+    @GetMapping("/info")
+    public ResponseEntity<BaseUserDto> findUser(@Validated @RequestBody BaseUserDto dto) {
+        var result = userService.findUserById(dto.getIdx());
+
+        return ResponseEntity.ok(userService.findUserById(dto.getIdx()));
+    }
+}
