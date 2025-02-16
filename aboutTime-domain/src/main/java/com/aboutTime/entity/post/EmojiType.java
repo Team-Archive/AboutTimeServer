b5199ed1f16bf91c9b012d4179ccb46cfac42b9e
kiteB <mmy789@sch.ac.kr>
2025-02-16 15:09:52 +0900

    refactor: EMOJI 정보 수정 - EMOJI_1, EMOJI_2, ... 에서 실제 이름으로 변경

--- aboutTime-domain/src/main/java/com/aboutTime/entity/post/EmojiType.java
+++ aboutTime-domain/src/main/java/com/aboutTime/entity/post/EmojiType.java
@@ -2,15 +2,15 @@ package com.aboutTime.entity.post;
 
 public enum EmojiType {
 
-    EMOJI_1,
-    EMOJI_2,
-    EMOJI_3,
-    EMOJI_4,
-    EMOJI_5,
-    EMOJI_6,
-    EMOJI_7,
-    EMOJI_8,
-    EMOJI_9,
-    EMOJI_10
+    FACE_WITH_TEARS_OF_JOY,
+    SMILING_FACE_WITH_HEART_EYES,
+    KISSING_FACE_WITH_CLOSED_EYES,
+    GRINNING_FACE,
+    LOUDLY_CRYING_FACE,
+    SMILING_FACE_WITH_HEARTS,
+    THUMBS_UP,
+    RED_HEART,
+    PARTY_POPPER,
+    CLAPPING_HANDS
 
 }
